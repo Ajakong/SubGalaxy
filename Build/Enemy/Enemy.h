260fdb3258eae05ca159a5b5d64ac9246095d2ba
Ajakong <135081816+Ajakong@users.noreply.github.com>
2024-09-03 13:56:55 +0900

    up

---
+++ Build/Enemy/Enemy.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "../MyLib/Physics/Collidable.h"
+
+class EnemyMover;
+class EnemyAI;
+
+class Enemy : public MyEngine :: Collidable
+{
+public:
+	Enemy(int handle,Priority priority,ObjectTag tag);
+	virtual ~Enemy();
+
+	virtual void Init() = 0;
+	virtual void Draw() = 0;
+	virtual void Update()=0;
+	//virtual int GetHP() { return m_Hp; }
+protected:
+	//int m_Hp;
+	int m_handle;
+
+};
+
---
+++ Build/MyLib/ObjectTag.h
@@ -0,0 +1,11 @@
+#pragma once
+
+enum class ObjectTag
+{
+	Stage,
+	Player,
+	Enemy,
+	Takobo,
+	EnemyAttack,
+	Object
+};
